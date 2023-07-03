6d45ac141ca60b36d346ba3fd20fc70401926fd7
pc29564530 <pc29564530@gmail.com>
2023-07-04 05:01:45 +0530

    converted speech to text

--- server.js
+++ server.js
@@ -1,56 +1,52 @@
 const express = require('express');
+const {Deepgram} = require('@deepgram/sdk');
 const app = express();
 const fs = require('fs');
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
-const {backOff} = require('exponential-backoff');
 
-const {Configuration, OpenAIApi} = require('openai');
+const deepgramApiKey = 'bdb4ae70b96dbc9938fd39947f90403837fc83d4';
+
+const mimetype = 'audio/wav';
 
 const io = new Server(server);
 
-
-//const config = new Configuration({apiKey: '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'});
-const config = new Configuration({apiKey: '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'});
-const openAI = new OpenAIApi(config);
-
- app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/frontend/index.html')
- });
+app.get('/',  (req,res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 
  io.on('connection', (socket) => {
-    socket.on('audio', async function(audioData)  {
-        fs.writeFileSync('audioFile.mp3', audioData);
-        socket.emit('audio', "audio file received");
-        console.log("audio file received");
-         console.log("enter into async function");
-         const text =   await convertSpeechToText('audioFile.mp3');
-         console.log(text);
-      console.log('text received');
-       
+    socket.on('audio', async (audioData) => {
+    fs.writeFileSync('audioFile.mp3', audioData, { encoding: 'binary', flag: 'w', mode: 0o666, mimetype: 'audio/mpeg' });
+    const convertText = await convertSpeechToText('audioFile.mp3');
+     //get the text and integrate the chatgpt and get the answer
+     // convert text to audio 
+     //play the audio file
     });
  });
 
- async function convertSpeechToText(str) {
-   console.log(str);
-   try {
-      const transcript = await openAI.createTranscription(fs.createReadStream(str), 'whisper-1'); 
-       let val = transcript.data.text;
-       console.log(transcript);
-       console.log(val);
-       setTimeout(() => {
-         return transcript.data.text;
-       }, 5000);
-   } catch (e) {
-     return  console.error('Unable to convert the audio to text',e)
-   }
-   
-   // console.log("Hello Deepak")
-   // console.log(transcript.data.text);
-   //return transcript.data.text;
+  async function convertSpeechToText(str) {
+  try {
+    const deepgram = new Deepgram(deepgramApiKey);
+    const convertedText = await deepgram.transcription.preRecorded(
+      { buffer: fs.readFileSync(str), mimetype },
+      { punctuate: true, language: 'en-US' },
+    );
+    const text = convertedText.results.channels[0].alternatives[0].transcript;
+    return text;
+    console.log(convertedText.results.channels[0].alternatives[0].transcript);
+  } catch (err) {
+    console.log(err.message);
+    return err;
+  }
+  
  }
+
+ 
 
  server.listen(3000, () => {
     console.log('server listening on port: 3000');
  })
+
+module.exports = app;
